6dccdfb0a1ee72303554d1090f39b1702277a208
Raysangar <raycosanchezgarcia@gmail.com>
2020-08-20 20:34:00 +0100

    Exchange rate API without date implemented

--- main.js
+++ main.js
@@ -12,5 +12,33 @@ handlers.GetCurrencyList = function (args, context) {
 
     // The pre-defined http object makes synchronous HTTP requests
     var response = http.request(url, httpMethod, content, contentType, headers);
-    return JSON.stringify(JSON.parse(response)["results"]);
+    return JSON.parse(response)["results"];
+};
+
+handlers.GetExchangeRate = function (args, context) {
+    var headers = {
+    };
+    
+    var body = {
+    };
+
+    var currencyFrom = args.from;
+    var currencyTo = args.to;
+
+    var conversion = currencyFrom + "_" + currencyTo;
+
+    
+    var url = "https://free.currconv.com/api/v7//api/v7/convert?q=" + conversion + "&compact=ultra&apiKey=a37ce2f41e2fecf090a2";
+    if (args.hasOwnProperty("date"))
+    {
+        debug.log(args["date"];
+        //url += "&date=[yyyy-mm-dd]";
+    }
+    var content = JSON.stringify(body);
+    var httpMethod = "get";
+    var contentType = "application/json";
+
+    // The pre-defined http object makes synchronous HTTP requests
+    var response = http.request(url, httpMethod, content, contentType, headers);
+    return JSON.parse(response)[conversion]["val"];
 };
