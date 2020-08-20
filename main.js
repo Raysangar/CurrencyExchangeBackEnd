8740e430e20e5340209eba89db8965130b29801c
Raysangar <raycosanchezgarcia@gmail.com>
2020-08-20 19:50:30 +0100

    Currency lists API implemented

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
+handlers.GetCurrencyList = function (args, context) {
+    var headers = {
+    };
+    
+    var body = {
+    };
 
+    var url = "https://free.currconv.com/api/v7/currencies?apiKey=a37ce2f41e2fecf090a2";
+    var content = JSON.stringify(body);
+    var httpMethod = "get";
+    var contentType = "application/json";
+
+    // The pre-defined http object makes synchronous HTTP requests
+    var response = http.request(url, httpMethod, content, contentType, headers);
+    return response.results.All;
+};
